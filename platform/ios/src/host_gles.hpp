315988ba43d230a407d7e1b37afec6db8f9ec816
Puissantsy <141499903+Puissantsy@users.noreply.github.com>
2026-09-19 15:27:13 +0200

    Expose host framebuffer statistics for v32

--- platform/ios/src/host_gles.hpp
+++ platform/ios/src/host_gles.hpp
@@ -9,6 +9,7 @@ extern "C" {
 bool PvZ2HostGLESBegin(std::uint32_t width, std::uint32_t height);
 std::uint32_t PvZ2HostGLESDefaultFramebuffer(void);
 const char* PvZ2HostGLESCapturePNG(void);
+const char* PvZ2HostGLESFrameStats(void);
 void PvZ2HostGLESEnd(void);
 
 #ifdef __cplusplus
