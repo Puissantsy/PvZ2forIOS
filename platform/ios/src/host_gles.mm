60a22874f434ae29f00b7d6b2d808e3a127f72a9
Puissantsy <141499903+Puissantsy@users.noreply.github.com>
2026-09-19 22:33:30 +0200

    v47: version framebuffer capture names

--- platform/ios/src/host_gles.mm
+++ platform/ios/src/host_gles.mm
@@ -688,13 +688,13 @@ PvZ2HostGLESCapturePNGNamed(
             ? [NSString
                 stringWithUTF8String:
                     file_name]
-            : @"pvz2-v46-frame.png";
+            : @"pvz2-v47-frame.png";
 
     if (safe_name.length == 0 ||
         [safe_name containsString:@"/"] ||
         [safe_name containsString:@"\\"]) {
         safe_name =
-            @"pvz2-v46-frame.png";
+            @"pvz2-v47-frame.png";
     }
 
     NSURL *target =
@@ -720,7 +720,7 @@ extern "C" const char*
 PvZ2HostGLESCapturePNG(void) {
     return
         PvZ2HostGLESCapturePNGNamed(
-            "pvz2-v46-final-frame.png");
+            "pvz2-v47-final-frame.png");
 }
 
 extern "C" void PvZ2HostGLESEnd(void) {
